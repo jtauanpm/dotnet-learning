2fcbc0be008f17f17b773e7df7a76a9faa615b25
jtauanpm <jtauan10@gmail.com>
2025-07-17 22:21:33 -0300

    add new entities

---
+++ ef-core/DevIO.IntroducaoEfCore/DevIO.IntroducaoEfCore.App/Domain/Cliente.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevIO.IntroducaoEfCore.App.Domain;
+
+// [Table("Clientes")]
+public class Cliente
+{
+    // [Key]
+    public int Id { get; set; }
+    // [Required]
+    public string Nome { get; set; }
+    // [Column("Phone")]
+    public string Telefone { get; set; }
+    public string CEP { get; set; }
+    public string Estado { get; set; }
+    public string Cidade { get; set; }
+}
