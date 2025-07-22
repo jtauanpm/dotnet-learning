46af4c38d85004bc4abfaaf9a2dba89b8115ee0b
jtauanpm <jtauan10@gmail.com>
2025-07-22 18:16:06 -0300

    enable DbContext sensitive data log

--- ef-core/DevIO.IntroducaoEfCore/DevIO.IntroducaoEfCore.App/Data/ApplicationContext.cs
+++ ef-core/DevIO.IntroducaoEfCore/DevIO.IntroducaoEfCore.App/Data/ApplicationContext.cs
@@ -1,16 +1,24 @@
 using DevIO.IntroducaoEfCore.App.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace DevIO.IntroducaoEfCore.App.Data;
 
 public class ApplicationContext : DbContext
 {
-    public DbSet<Pedido> Pedido { get; set; }
+    private static readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(p => p.AddConsole());
+    public DbSet<Pedido> Pedidos { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        //TODO: place it on a secret store
-        optionsBuilder.UseSqlServer();
+        
+        optionsBuilder
+            .UseLoggerFactory(_loggerFactory)
+            .EnableSensitiveDataLogging()
+            //TODO: place it on a secret store
+            .UseSqlServer();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
