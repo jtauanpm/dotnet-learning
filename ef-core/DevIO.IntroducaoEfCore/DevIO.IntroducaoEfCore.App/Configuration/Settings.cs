afc8bf6e6fb8d8af76a32f0943a36221a5fdf93d
jtauanpm <jtauan10@gmail.com>
2025-07-24 23:31:32 -0300

    add settings store

---
+++ ef-core/DevIO.IntroducaoEfCore/DevIO.IntroducaoEfCore.App/Configuration/Settings.cs
@@ -0,0 +1,8 @@
+using Microsoft.Extensions.Configuration;
+
+namespace DevIO.IntroducaoEfCore.App.Configuration;
+
+public static class Settings
+{
+    public static IConfiguration Configuration { get; set; }
+}
