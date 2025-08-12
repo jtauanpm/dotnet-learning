0462d06e67fd63c9c94c6546f69c7a10f60edac2
jtauanpm <jtauan10@gmail.com>
2025-08-11 21:55:57 -0300

    add asp.net identity migrations

--- asp.net-core/DevIO.PrimeiraApi/DevIO.PrimeiraApi.App/Migrations/20250810030150_Add_Identity.Designer.cs
+++ asp.net-core/DevIO.PrimeiraApi/DevIO.PrimeiraApi.App/Migrations/20250810030150_Add_Identity.Designer.cs
@@ -4,6 +4,7 @@ using DevIO.PrimeiraApi.App.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevIO.PrimeiraApi.App.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250810030150_Add_Identity")]
+    partial class Add_Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
