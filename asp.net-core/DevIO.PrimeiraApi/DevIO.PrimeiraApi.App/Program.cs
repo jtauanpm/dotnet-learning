c25c6ef9ac4121fbb1e9b9f1ac09666c64f3d88b
jtauanpm <jtauan10@gmail.com>
2025-08-14 22:01:21 -0300

    update Program.cs with extension methods

--- asp.net-core/DevIO.PrimeiraApi/DevIO.PrimeiraApi.App/Program.cs
+++ asp.net-core/DevIO.PrimeiraApi/DevIO.PrimeiraApi.App/Program.cs
@@ -1,92 +1,14 @@
-using System.Text;
-using DevIO.PrimeiraApi.App.DTOs;
-using DevIO.PrimeiraApi.App.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using DevIO.PrimeiraApi.App.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options =>
-    {
-        // options.SuppressModelStateInvalidFilter = true;
-    });
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Development", devBuilder => devBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-    options.AddPolicy("Production", prodBuilder => prodBuilder.WithOrigins("https://localhost:9000").WithMethods("POST").AllowAnyHeader());
-});
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.ApiKey,
-        Description = "Insira o seu token de acesso",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Scheme = "Bearer",
-        BearerFormat = "JWT"
-    });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            }, []
-        }
-    });
-});
-
-builder.Services.AddDbContext<ApiDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Dev_IO_PrimeiraAPI")));
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApiDbContext>();
-
-var jwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
-
-// Registra a seção como configuração tipada (classe JwtSettings)
-// Isso permite injetar IOptions<JwtSettings> em outros lugares da aplicação
-builder.Services.Configure<JwtSettings>(jwtSettingsSection);
-
-// TODO: extract extension method 
-var jwtSettings = jwtSettingsSection.Get<JwtSettings>();
-var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.RequireHttpsMetadata = false;
-    options.SaveToken = true;
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = jwtSettings.Audiencia,
-        ValidIssuer = jwtSettings.Emissor
-    };
-});
+builder.AddApiConfig()
+    .AddCorsConfig()
+    .AddSwaggerConfig()
+    .AddEfCoreConfig()
+    .AddIdentityConfig();
 
 var app = builder.Build();
 
