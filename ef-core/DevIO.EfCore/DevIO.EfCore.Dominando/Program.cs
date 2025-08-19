cdff6e9496f34416928fe7a42344daf48959feda
jtauanpm <jtauan10@gmail.com>
2025-08-18 22:15:12 -0300

    Db EnsureCreated and Deleted

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Program.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Program.cs
@@ -1,10 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using DevIO.EfCore.Dominando.Configuration;
+using DevIO.EfCore.Dominando.Data;
 using Microsoft.Extensions.Configuration;
 
 Settings.Configuration = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
     .Build();
+
+EnsureCreatedAndDeleted();
+return;
+
+static void EnsureCreatedAndDeleted()
+{
+    using var db = new ApplicationDbContext();
+    db.Database.EnsureCreated();
+
+    db.Database.EnsureDeleted();
+}
 
 
