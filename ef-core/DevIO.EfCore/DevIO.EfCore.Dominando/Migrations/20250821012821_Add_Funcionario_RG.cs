9d7304397cec093af6bd034776b2e5f2a7039fd6
jtauanpm <jtauan10@gmail.com>
2025-08-21 22:43:41 -0300

    add migrations

---
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Migrations/20250821012821_Add_Funcionario_RG.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevIO.EfCore.Dominando.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Funcionario_RG : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RG",
+                table: "Funcionarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RG",
+                table: "Funcionarios");
+        }
+    }
+}
