7487a36844a4de06ef2bb2020a726f7e2056e0ea
jtauanpm <jtauan10@gmail.com>
2025-08-28 17:38:46 -0300

    lazy load by inject ILazyLoader

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Domain/Departamento.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Domain/Departamento.cs
@@ -1,46 +1,48 @@
-namespace DevIO.EfCore.Dominando.Domain
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace DevIO.EfCore.Dominando.Domain;
+
+public class Departamento
 {
-    public class Departamento
+    public int Id { get; set; }
+    public string Descricao { get; set; }
+    public bool Ativo { get; set; }
+    // public List<Funcionario> Funcionarios { get; set; }
+    public Departamento()
     {
-        public int Id { get; set; }
-        public string Descricao { get; set; }
-        public bool Ativo { get; set; }
-
-        public Departamento()
-        {
-        }
-
-        private Action<object,string> _lazyLoader {get;set;}
-        private Departamento(Action<object,string> lazyLoader)
-        {
-            _lazyLoader = lazyLoader;
-        }
-
-        private List<Funcionario> _funcionarios;
-        public List<Funcionario> Funcionarios 
-        {
-            get
-            {
-                _lazyLoader?.Invoke(this, nameof(Funcionarios));
-
-                return _funcionarios;
-            } 
-            set => _funcionarios = value;
-        }
-
-        /*
-        private ILazyLoader _lazyLoader {get;set;}
-        private Departamento(ILazyLoader lazyLoader)
-        {
-            _lazyLoader = lazyLoader;
-        }
-
-        private List<Funcionario> _funcionarios;
-        public List<Funcionario> Funcionarios 
-        {
-            get => _lazyLoader.Load(this, ref _funcionarios);
-            set => _funcionarios = value;
-        }
-        */
     }
+
+    private ILazyLoader _lazyLoader { get; set; }
+    private Departamento(ILazyLoader lazyLoader)
+    {
+        _lazyLoader = lazyLoader;
+    }
+
+    private List<Funcionario>? _funcionarios;
+    public List<Funcionario>? Funcionarios
+    {
+        get => _lazyLoader.Load(this, ref _funcionarios);
+        set => _funcionarios = value;
+    }
+    
+    /*
+    private Action<object, string> _lazyLoader { get; set; }
+    private Departamento(Action<object, string> lazyLoader)
+    {
+        _lazyLoader = lazyLoader;
+    }
+
+    private List<Funcionario> _funcionarios;
+    public virtual List<Funcionario> Funcionarios
+    {
+        get
+        {
+            _lazyLoader?.Invoke(this, nameof(Funcionarios));
+            return _funcionarios;
+        }
+
+        set => _funcionarios = value;
+    }
+    */
+
 }
