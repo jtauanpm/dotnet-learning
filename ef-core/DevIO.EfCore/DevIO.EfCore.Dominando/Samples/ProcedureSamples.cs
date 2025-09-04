0a62f133d5815aac78e6cb203d9994ae19865ef4
jtauanpm <jtauan10@gmail.com>
2025-09-04 20:35:26 -0300

    learning procedures by ef core

---
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Samples/ProcedureSamples.cs
@@ -0,0 +1,26 @@
+using DevIO.EfCore.Dominando.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.EfCore.Dominando.Samples;
+
+public static class ProcedureSamples
+{
+    public static void CriarStoredProcedure()
+    {
+        var criarDepartamento = """
+
+                                        CREATE OR ALTER PROCEDURE CriarDepartamento
+                                            @Descricao VARCHAR(100),
+                                            @Ativo bit
+                                        AS
+                                        BEGIN
+                                            INSERT INTO
+                                               Departamentos(Descricao, Ativo, Excluido)
+                                            VALUES (@Descricao, @Ativo, 0)
+                                        END
+                                """;
+        
+        using var dbContext = new ApplicationDbContext();
+        dbContext.Database.ExecuteSqlRaw(criarDepartamento);
+    }
+}
