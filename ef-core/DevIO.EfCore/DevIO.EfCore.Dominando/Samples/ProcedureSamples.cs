7681299012a7e3a77cfdd40a2e5ad8ac862733f1
jtauanpm <jtauan10@gmail.com>
2025-09-04 20:37:08 -0300

    using procedure

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Samples/ProcedureSamples.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Samples/ProcedureSamples.cs
@@ -5,6 +5,12 @@ namespace DevIO.EfCore.Dominando.Samples;
 
 public static class ProcedureSamples
 {
+    public static void InserirDadosProcedure()
+    {
+        using var dbContext = new ApplicationDbContext();
+        dbContext.Database.ExecuteSqlRaw("execute CriarDepartamento @p0, @p1", "Departamento Via Procedure", true);
+    }
+    
     public static void CriarStoredProcedure()
     {
         var criarDepartamento = """
