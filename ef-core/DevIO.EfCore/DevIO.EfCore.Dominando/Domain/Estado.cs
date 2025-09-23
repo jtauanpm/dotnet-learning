0b973da6dffee4913a5e46299a5b41512dcba010
jtauanpm <jtauan10@gmail.com>
2025-09-22 21:53:55 -0300

    add Estado and Governador

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Domain/Estado.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Domain/Estado.cs
@@ -4,4 +4,17 @@ public class Estado
 {
     public int Id { get; set; }
     public string Nome { get; set; }
+
+    public Governador Governador { get; set; }
+}
+
+public class Governador
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Partido { get; set; }
+    
+    public int EstadoId { get; set; }
+    public Estado Estado { get; set; }
 }
