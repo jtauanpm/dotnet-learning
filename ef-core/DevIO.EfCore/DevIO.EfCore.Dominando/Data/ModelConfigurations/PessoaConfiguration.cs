b469be771d7edde1670197a3922c1b9e092a7846
jtauanpm <jtauan10@gmail.com>
2025-09-28 19:54:23 -0300

    TPH TPT

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Data/ModelConfigurations/PessoaConfiguration.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Data/ModelConfigurations/PessoaConfiguration.cs
@@ -1,6 +1,18 @@
-namespace DevIO.EfCore.Dominando.Configurations;
+using DevIO.EfCore.Dominando.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class PessoaConfiguration
+namespace DevIO.EfCore.Dominando.Data.ModelConfigurations;
+
+public class PessoaConfiguration : IEntityTypeConfiguration<Pessoa>
 {
-    
+    public void Configure(EntityTypeBuilder<Pessoa> builder)
+    {
+        builder
+            .ToTable("Pessoas")
+            .HasDiscriminator<int>("TipoPessoa")
+            .HasValue<Pessoa>(3)
+            .HasValue<Instrutor>(6)
+            .HasValue<Pessoa>(99);
+    }
 }
---
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/Domain/Pessoa.cs
@@ -0,0 +1,16 @@
+namespace DevIO.EfCore.Dominando.Domain;
+
+public class Pessoa
+{
+    
+}
+
+public class Instrutor
+{
+    
+}
+
+public class Aluno
+{
+    
+}
