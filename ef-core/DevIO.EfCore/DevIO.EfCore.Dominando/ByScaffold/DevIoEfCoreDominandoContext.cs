d1a13ae5bd5c0c121677e5e6ee91a76202fd4bf9
jtauanpm <jtauan10@gmail.com>
2025-10-17 19:36:02 -0300

    ...

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/ByScaffold/DevIoEfCoreDominandoContext.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando/ByScaffold/DevIoEfCoreDominandoContext.cs
@@ -45,7 +45,7 @@ public partial class DevIoEfCoreDominandoContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Dev_IO_EfCore_Dominando;User Id=sa;Password=Jordanna123.;Encrypt=False;MultipleActiveResultSets=True");
+        => optionsBuilder.UseSqlServer("");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
