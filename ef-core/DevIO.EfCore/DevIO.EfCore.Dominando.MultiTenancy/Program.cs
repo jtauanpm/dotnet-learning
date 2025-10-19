11495ec8e9bc9ef0148b417e11ce383725618d12
jtauanpm <jtauan10@gmail.com>
2025-10-18 21:00:12 -0300

    user secret, database setup and initialize

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando.MultiTenancy/Program.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando.MultiTenancy/Program.cs
@@ -1,3 +1,7 @@
+using DevIO.EfCore.Dominando.MultiTenancy.EFCore;
+using DevIO.EfCore.Dominando.MultiTenancy.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var connectionString = builder.Configuration.GetConnectionString("Dev_IO_EfCore_Dominando_MultiTenancy");
+builder.Services.AddDbContext<MultiTenancyDbContext>(o => 
+    o.UseSqlServer(connectionString)
+    .EnableSensitiveDataLogging()
+    .LogTo(Console.WriteLine));
+
 var app = builder.Build();
+
+DatabaseInitialize(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,3 +33,23 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void DatabaseInitialize(IApplicationBuilder appBuilder)
+{
+    using var db = appBuilder
+        .ApplicationServices
+        .CreateScope()
+        .ServiceProvider
+        .GetRequiredService<MultiTenancyDbContext>();
+    
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+
+    for (var i = 0; i < 5; i++)
+    {
+        db.People.Add(new Person { Name = $"Person {i}" });
+        db.Products.Add(new Product { Description = $"Product {i}" });
+    }
+
+    db.SaveChanges();
+}
