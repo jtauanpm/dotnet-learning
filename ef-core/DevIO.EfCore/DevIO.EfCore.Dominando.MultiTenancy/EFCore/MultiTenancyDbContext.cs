4fb0c103dfc6c3105b180fa4023242d8036e343d
jtauanpm <jtauan10@gmail.com>
2025-10-22 20:49:18 -0300

    global query by TenantId

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando.MultiTenancy/EFCore/MultiTenancyDbContext.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando.MultiTenancy/EFCore/MultiTenancyDbContext.cs
@@ -1,4 +1,5 @@
 using DevIO.EfCore.Dominando.MultiTenancy.Entities;
+using DevIO.EfCore.Dominando.MultiTenancy.Providers;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevIO.EfCore.Dominando.MultiTenancy.EFCore;
@@ -7,8 +8,28 @@ public class MultiTenancyDbContext : DbContext
 {
     public DbSet<Person> People { get; set; }
     public DbSet<Product> Products { get; set; }
+    
+    private readonly TenantDataProvider _tenantDataProvider;
 
-    public MultiTenancyDbContext(DbContextOptions<MultiTenancyDbContext> options) : base(options)
+    public MultiTenancyDbContext(DbContextOptions<MultiTenancyDbContext> options, TenantDataProvider tenantDataProvider) : base(options)
     {
+        _tenantDataProvider = tenantDataProvider;
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>().HasData(
+            new Person { Id = 1, Name = "Person 1", TenantId = "tenant-1" },
+            new Person { Id = 2, Name = "Person 2", TenantId = "tenant-2" },
+            new Person { Id = 3, Name = "Person 3", TenantId = "tenant-2" });
+        
+        modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, Description = "Description 1", TenantId = "tenant-1" },
+            new Product { Id = 2, Description = "Description 2", TenantId = "tenant-2" },
+            new Product { Id = 3, Description = "Description 3", TenantId = "tenant-2" });
+        
+        //TODO: do this dynamically for all entities that inherit from BaseEntity
+        modelBuilder.Entity<Person>().HasQueryFilter(e => e.TenantId == _tenantDataProvider.TenantId);
+        modelBuilder.Entity<Product>().HasQueryFilter(e => e.TenantId == _tenantDataProvider.TenantId);
     }
 }
