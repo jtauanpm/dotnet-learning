88caa6938199a2cb8f28106b3a4b5196322128ee
jtauanpm <jtauan10@gmail.com>
2025-10-24 20:27:15 -0300

    multi tenancy schema

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando.MultiTenancy/Program.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando.MultiTenancy/Program.cs
@@ -1,8 +1,10 @@
 using DevIO.EfCore.Dominando.MultiTenancy.EFCore;
+using DevIO.EfCore.Dominando.MultiTenancy.EFCore.Interceptors;
 using DevIO.EfCore.Dominando.MultiTenancy.Entities;
 using DevIO.EfCore.Dominando.MultiTenancy.Middlewares;
 using DevIO.EfCore.Dominando.MultiTenancy.Providers;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,17 +14,33 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// builder.Services.AddScoped<StrategySchemaInterceptor>();
+
 var connectionString = builder.Configuration.GetConnectionString("Dev_IO_EfCore_Dominando_MultiTenancy");
-builder.Services.AddDbContext<MultiTenancyDbContext>(o => 
-    o.UseSqlServer(connectionString)
-    .EnableSensitiveDataLogging()
-    .LogTo(Console.WriteLine));
+// builder.Services.AddDbContext<MultiTenancyDbContext>(o => 
+//     o.UseSqlServer(connectionString)
+//     .EnableSensitiveDataLogging()
+//     .LogTo(Console.WriteLine));
+
+builder.Services.AddDbContext<MultiTenancyDbContext>((provider, options) =>
+{
+    options.UseSqlServer(connectionString)
+        .EnableSensitiveDataLogging()
+        // For using custom cache model, caching by TenantData for reusing
+        .ReplaceService<IModelCacheKeyFactory, ModelCacheKeyFactory>()
+        .LogTo(Console.WriteLine);
+    
+    // We retrieve instance at runtime so that TenantDataProvider is resolved
+    // var interceptor = provider.GetRequiredService<StrategySchemaInterceptor>();
+    // options.AddInterceptors(interceptor);
+});
 
 builder.Services.AddScoped<TenantDataProvider>();
+builder.Services.AddTransient<TenantResolverMiddleware>();
 
 var app = builder.Build();
 
-DatabaseInitialize(app);
+// DatabaseInitialize(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
