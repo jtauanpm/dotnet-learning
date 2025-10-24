92bca2ec40638ff9abcec8448cbc2c459e3d0a12
jtauanpm <jtauan10@gmail.com>
2025-10-23 21:47:23 -0300

    controllers by tenants

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando.MultiTenancy/Controllers/PersonController.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando.MultiTenancy/Controllers/PersonController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DevIO.EfCore.Dominando.MultiTenancy.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("{tenant}/[controller]")]
 public class PersonController : ControllerBase
 {
     private readonly ILogger<PersonController> _logger;
