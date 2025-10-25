5190d8b56a2c1c7d90befb9c7d48d2ba7abe4c49
jtauanpm <jtauan10@gmail.com>
2025-10-25 16:30:04 -0300

    setup

--- ef-core/DevIO.EfCore/DevIO.EfCore.Dominando.MultiTenancy/Program.cs
+++ ef-core/DevIO.EfCore/DevIO.EfCore.Dominando.MultiTenancy/Program.cs
@@ -1,6 +1,7 @@
 using DevIO.EfCore.Dominando.MultiTenancy.EFCore;
 using DevIO.EfCore.Dominando.MultiTenancy.EFCore.Interceptors;
 using DevIO.EfCore.Dominando.MultiTenancy.Entities;
+using DevIO.EfCore.Dominando.MultiTenancy.Extensions;
 using DevIO.EfCore.Dominando.MultiTenancy.Middlewares;
 using DevIO.EfCore.Dominando.MultiTenancy.Providers;
 using Microsoft.EntityFrameworkCore;
@@ -17,26 +18,48 @@ builder.Services.AddOpenApi();
 // builder.Services.AddScoped<StrategySchemaInterceptor>();
 
 var connectionString = builder.Configuration.GetConnectionString("Dev_IO_EfCore_Dominando_MultiTenancy");
+
+// Strategy 1 - TenantId column identifier for each table
 // builder.Services.AddDbContext<MultiTenancyDbContext>(o => 
 //     o.UseSqlServer(connectionString)
 //     .EnableSensitiveDataLogging()
 //     .LogTo(Console.WriteLine));
 
-builder.Services.AddDbContext<MultiTenancyDbContext>((provider, options) =>
+// Strategy 2 - spliting by Schema - TenantId (using ModelCache by Schema or using command interceptor to add schema in queries)
+// builder.Services.AddDbContext<MultiTenancyDbContext>((provider, options) =>
+// {
+//     options.UseSqlServer(connectionString)
+//         .EnableSensitiveDataLogging()
+//         // For using custom cache model, caching by TenantData for reusing
+//         .ReplaceService<IModelCacheKeyFactory, ModelCacheKeyFactory>()
+//         .LogTo(Console.WriteLine);
+//     
+//     // We retrieve instance at runtime so that TenantDataProvider is resolved
+//     // var interceptor = provider.GetRequiredService<StrategySchemaInterceptor>();
+//     // options.AddInterceptors(interceptor);
+// });
+// builder.Services.AddTransient<TenantResolverMiddleware>();
+// builder.Services.AddScoped<TenantDataProvider>();
+
+// Strategy 3 - A DB for each Tenant
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<MultiTenancyDbContext>(provider =>
 {
-    options.UseSqlServer(connectionString)
-        .EnableSensitiveDataLogging()
-        // For using custom cache model, caching by TenantData for reusing
-        .ReplaceService<IModelCacheKeyFactory, ModelCacheKeyFactory>()
-        .LogTo(Console.WriteLine);
+    var optionsBuilder = new DbContextOptionsBuilder<MultiTenancyDbContext>();
     
-    // We retrieve instance at runtime so that TenantDataProvider is resolved
-    // var interceptor = provider.GetRequiredService<StrategySchemaInterceptor>();
-    // options.AddInterceptors(interceptor);
+    var httpContext = provider.GetService<IHttpContextAccessor>()?.HttpContext;
+    var tenantId = httpContext?.ExtractTenantIdFromPath();
+    
+    var connectionString = builder.Configuration.GetConnectionString(tenantId);
+    
+    optionsBuilder
+        .UseSqlServer(connectionString)
+        .LogTo(Console.WriteLine)
+        .EnableSensitiveDataLogging();
+    
+    return new MultiTenancyDbContext(optionsBuilder.Options);
 });
 
-builder.Services.AddScoped<TenantDataProvider>();
-builder.Services.AddTransient<TenantResolverMiddleware>();
 
 var app = builder.Build();
 
