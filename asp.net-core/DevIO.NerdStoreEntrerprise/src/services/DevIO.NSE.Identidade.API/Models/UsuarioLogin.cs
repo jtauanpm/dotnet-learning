887c787f2bf8a6e5ec4d70d83a6b51cbd223561b
jtauanpm <jtauan10@gmail.com>
2025-11-17 20:41:53 -0300

    user login and register DTOs

---
+++ asp.net-core/DevIO.NerdStoreEntrerprise/src/services/DevIO.NSE.Identidade.API/Models/UsuarioLogin.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.NSE.Identidade.API.Models;
+
+public class UsuarioLogin
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} não é um e-mail válido")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; }
+}
---
+++ asp.net-core/DevIO.NerdStoreEntrerprise/src/services/DevIO.NSE.Identidade.API/Models/UsuarioRegistro.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.NSE.Identidade.API.Models;
+
+public class UsuarioRegistro
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} não é um e-mail válido")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; }
+
+    [Compare("Senha", ErrorMessage = "As senhas não conferem")]
+    public string SenhaConfirmacao { get; set; }
+}
