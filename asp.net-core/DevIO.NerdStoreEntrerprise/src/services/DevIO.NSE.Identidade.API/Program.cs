454fc65fc67c715150f8118dcc2954b2544be8d5
jtauanpm <jtauan10@gmail.com>
2025-11-19 21:23:52 -0300

    add swagger doc

--- asp.net-core/DevIO.NerdStoreEntrerprise/src/services/DevIO.NSE.Identidade.API/Program.cs
+++ asp.net-core/DevIO.NerdStoreEntrerprise/src/services/DevIO.NSE.Identidade.API/Program.cs
@@ -1,6 +1,7 @@
 using DevIO.NSE.Identidade.API.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Dev_IO_NerdStoreEnterprise_Identidade");
@@ -15,8 +16,21 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "NerdStore Enterprise Identity API",
+        Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications.",
+        Contact = new OpenApiContact {Name = "Jonathan Maia", Email = "some@email.com"},
+        License = new OpenApiLicense {Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT")}
+    });
+});
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"));
 
 if (app.Environment.IsDevelopment())
 {
