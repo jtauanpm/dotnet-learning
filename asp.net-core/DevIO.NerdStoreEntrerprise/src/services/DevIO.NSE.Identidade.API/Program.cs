47653423c47f32dfaa7bf898df54118e9aafad65
jtauanpm <jtauan10@gmail.com>
2025-11-22 00:21:24 -0300

    refactor identity config and add jwt

--- asp.net-core/DevIO.NerdStoreEntrerprise/src/services/DevIO.NSE.Identidade.API/Program.cs
+++ asp.net-core/DevIO.NerdStoreEntrerprise/src/services/DevIO.NSE.Identidade.API/Program.cs
@@ -1,10 +1,6 @@
 using DevIO.NSE.Identidade.API.Configuration;
 using DevIO.NSE.Identidade.API.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Dev_IO_NerdStoreEnterprise_Identidade");
@@ -12,10 +8,7 @@ var connectionString = builder.Configuration.GetConnectionString("Dev_IO_NerdSto
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
+builder.Services.AddIdentityConfiguration(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
