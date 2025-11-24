e5fa418935769a0053cceb043ff25e582a2b52a1
jtauanpm <jtauan10@gmail.com>
2025-11-24 19:43:57 -0300

    update auth controller

--- asp.net-core/DevIO.NerdStoreEntrerprise/src/services/DevIO.NSE.Identidade.API/Controllers/AuthController.cs
+++ asp.net-core/DevIO.NerdStoreEntrerprise/src/services/DevIO.NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,63 +1,143 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using DevIO.NSE.Identidade.API.Configuration;
 using DevIO.NSE.Identidade.API.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DevIO.NSE.Identidade.API.Controllers;
 
-[ApiController]
 [Route("api/identidade")]
-public class AuthController : ControllerBase
+public class AuthController : MainController
 {
-    private readonly UserManager<IdentityUser> _userManager;
     private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly AppSettings _appSettings;
 
-    public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    public AuthController(SignInManager<IdentityUser> signInManager, 
+        UserManager<IdentityUser> userManager,
+        IOptions<AppSettings> appSettings)
     {
-        _userManager = userManager;
         _signInManager = signInManager;
+        _userManager = userManager;
+        _appSettings = appSettings.Value;
     }
 
-    [HttpPost("register")]
+    [HttpPost("nova-conta")]
     public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
     {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest();
-        }
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
 
         var user = new IdentityUser
         {
             UserName = usuarioRegistro.Email,
-            Email = usuarioRegistro.Email
+            Email = usuarioRegistro.Email,
+            EmailConfirmed = true
         };
-        
+
         var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
 
         if (result.Succeeded)
         {
-            await _signInManager.SignInAsync(user, false);
-            return Ok();
+            return CustomResponse(await GerarJwt(usuarioRegistro.Email));
         }
-        
-        return BadRequest(result.Errors);
+
+        foreach (var error in result.Errors)
+        {
+            AdicionarErroProcessamento(error.Description);
+        }
+
+        return CustomResponse();
     }
 
-    [HttpPost("login")]
+    [HttpPost("autenticar")]
     public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
     {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest();
-        }
-        
+        if (!ModelState.IsValid) return CustomResponse(ModelState);
+
         var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha,
             false, true);
 
         if (result.Succeeded)
         {
-            return Ok();
+            return CustomResponse(await GerarJwt(usuarioLogin.Email));
         }
 
-        return BadRequest();
+        if (result.IsLockedOut)
+        {
+            AdicionarErroProcessamento("Usuário temporariamente bloqueado por tentativas inválidas");
+            return CustomResponse();
+        }
+
+        AdicionarErroProcessamento("Usuário ou Senha incorretos");
+        return CustomResponse();
     }
+
+    private async Task<LoginOutput> GerarJwt(string email)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+        var claims = await _userManager.GetClaimsAsync(user);
+
+        var identityClaims = await ObterClaimsUsuario(claims, user);
+        var encodedToken = CodificarToken(identityClaims);
+
+        return ObterRespostaToken(encodedToken, user, claims);
+    }
+
+    private async Task<ClaimsIdentity> ObterClaimsUsuario(ICollection<Claim> claims, IdentityUser user)
+    {
+        var userRoles = await _userManager.GetRolesAsync(user);
+
+        claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+        foreach (var userRole in userRoles)
+        {
+            claims.Add(new Claim("role", userRole));
+        }
+
+        var identityClaims = new ClaimsIdentity();
+        identityClaims.AddClaims(claims);
+
+        return identityClaims;
+    }
+
+    private string CodificarToken(ClaimsIdentity identityClaims)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_appSettings.Segredo);
+        var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+        {
+            Issuer = _appSettings.Emissor,
+            Audience = _appSettings.Audiencia,
+            Subject = identityClaims,
+            Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        });
+
+        return tokenHandler.WriteToken(token);
+    }
+
+    private LoginOutput ObterRespostaToken(string encodedToken, IdentityUser user, IEnumerable<Claim> claims)
+    {
+        return new LoginOutput
+        {
+            AccessToken = encodedToken,
+            ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+            UsuarioToken = new UserToken
+            {
+                Id = user.Id,
+                Email = user.Email!,
+                Claims = claims.Select(c => new UserClaim { Type = c.Type, Value = c.Value })
+            }
+        };
+    }
+
+    private static long ToUnixEpochDate(DateTime date)
+        => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
 }
